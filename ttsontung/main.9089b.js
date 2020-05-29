77b7e2b8057ed6b6efa5b03cd436b299032828e7
matthieubrossard <60191207+matthieubrossard@users.noreply.github.com>
2020-05-29 17:09:53 +0700

    + add LongNote

--- ttsontung/main.9089b.js
+++ ttsontung/main.9089b.js
@@ -142,7 +142,7 @@ console.log('=============lcton======');
     // jsList
     var jsList = settings.jsList;
 
-    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.ab65f.js';
+    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.4e59c.js';
     if (jsList) {
         jsList = jsList.map(function (x) {
             return 'src/' + x;
@@ -180,7 +180,7 @@ console.log('=============lcton======');
 if (window.jsb) {
     var isRuntime = (typeof loadRuntime === 'function');
     if (isRuntime) {
-        require('src/settings.e1c1a.js');
+        require('src/settings.ac1f6.js');
         require('src/cocos2d-runtime.js');
         if (CC_PHYSICS_BUILTIN || CC_PHYSICS_CANNON) {
             require('src/physics.js');
@@ -188,7 +188,7 @@ if (window.jsb) {
         require('jsb-adapter/engine/index.js');
     }
     else {
-        require('src/settings.e1c1a.js');
+        require('src/settings.ac1f6.js');
         require('src/cocos2d-jsb.js');
         if (CC_PHYSICS_BUILTIN || CC_PHYSICS_CANNON) {
             require('src/physics.js');
