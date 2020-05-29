45c80251285863690c06d481e0409c5a4b5aa3da
matthieubrossard <60191207+matthieubrossard@users.noreply.github.com>
2020-05-29 17:20:41 +0700

    + add

--- ttsontung/main.6498f.js
+++ ttsontung/main.6498f.js
@@ -142,7 +142,7 @@ console.log('=============lcton======');
     // jsList
     var jsList = settings.jsList;
 
-    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.8c03b.js';
+    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.46c41.js';
     if (jsList) {
         jsList = jsList.map(function (x) {
             return 'src/' + x;
