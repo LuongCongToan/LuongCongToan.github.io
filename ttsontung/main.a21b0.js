cb1c0bfab95c0606f0c5d48770b7c5a22e202690
matthieubrossard <60191207+matthieubrossard@users.noreply.github.com>
2020-06-04 16:07:03 +0700

    - new

--- ttsontung/main.a21b0.js
+++ ttsontung/main.a21b0.js
@@ -142,7 +142,7 @@ console.log('=============lcton======');
     // jsList
     var jsList = settings.jsList;
 
-    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.4ea55.js';
+    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.46445.js';
     if (jsList) {
         jsList = jsList.map(function (x) {
             return 'src/' + x;
@@ -180,7 +180,7 @@ console.log('=============lcton======');
 if (window.jsb) {
     var isRuntime = (typeof loadRuntime === 'function');
     if (isRuntime) {
-        require('src/settings.c164a.js');
+        require('src/settings.e00a5.js');
         require('src/cocos2d-runtime.js');
         if (CC_PHYSICS_BUILTIN || CC_PHYSICS_CANNON) {
             require('src/physics.js');
@@ -188,7 +188,7 @@ if (window.jsb) {
         require('jsb-adapter/engine/index.js');
     }
     else {
-        require('src/settings.c164a.js');
+        require('src/settings.e00a5.js');
         require('src/cocos2d-jsb.js');
         if (CC_PHYSICS_BUILTIN || CC_PHYSICS_CANNON) {
             require('src/physics.js');
