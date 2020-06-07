90859297de22c818c2adb75d9a5f0f772c835451
LuongCongToan <pt.team2011@gmail.com>
2020-06-07 08:34:06 +0700

    - new

--- ttsontung/main.92636.js
+++ ttsontung/main.92636.js
@@ -141,7 +141,7 @@ window.boot = function () {
     // jsList
     var jsList = settings.jsList;
 
-    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.7514f.js';
+    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.61e95.js';
     if (jsList) {
         jsList = jsList.map(function (x) {
             return 'src/' + x;
@@ -179,7 +179,7 @@ window.boot = function () {
 if (window.jsb) {
     var isRuntime = (typeof loadRuntime === 'function');
     if (isRuntime) {
-        require('src/settings.cc062.js');
+        require('src/settings.f7a78.js');
         require('src/cocos2d-runtime.js');
         if (CC_PHYSICS_BUILTIN || CC_PHYSICS_CANNON) {
             require('src/physics.js');
@@ -187,7 +187,7 @@ if (window.jsb) {
         require('jsb-adapter/engine/index.js');
     }
     else {
-        require('src/settings.cc062.js');
+        require('src/settings.f7a78.js');
         require('src/cocos2d-jsb.js');
         if (CC_PHYSICS_BUILTIN || CC_PHYSICS_CANNON) {
             require('src/physics.js');
