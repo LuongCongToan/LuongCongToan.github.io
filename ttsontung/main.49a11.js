62d734aeee6900c5d7ae4040977b9702758a97c9
toan.luongcong <60191207+toan.luongcong@users.noreply.github.com>
2020-06-12 11:34:52 +0700

    - updated

--- ttsontung/main.49a11.js
+++ ttsontung/main.49a11.js
@@ -1,4 +1,5 @@
 window.boot = function () {
+console.log('=============lcton======');
     var settings = window._CCSettings;
     window._CCSettings = undefined;
 
@@ -135,13 +136,13 @@ window.boot = function () {
 
         // load scene
         loadScene(launchScene);
-
+		console.log('=============lcton======');
     };
 
     // jsList
     var jsList = settings.jsList;
 
-    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.2ba55.js';
+    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.93813.js';
     if (jsList) {
         jsList = jsList.map(function (x) {
             return 'src/' + x;
@@ -179,7 +180,7 @@ window.boot = function () {
 if (window.jsb) {
     var isRuntime = (typeof loadRuntime === 'function');
     if (isRuntime) {
-        require('src/settings.24296.js');
+        require('src/settings.c779f.js');
         require('src/cocos2d-runtime.js');
         if (CC_PHYSICS_BUILTIN || CC_PHYSICS_CANNON) {
             require('src/physics.js');
@@ -187,7 +188,7 @@ if (window.jsb) {
         require('jsb-adapter/engine/index.js');
     }
     else {
-        require('src/settings.24296.js');
+        require('src/settings.c779f.js');
         require('src/cocos2d-jsb.js');
         if (CC_PHYSICS_BUILTIN || CC_PHYSICS_CANNON) {
             require('src/physics.js');
