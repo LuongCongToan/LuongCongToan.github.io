220688a840cf1576635c5906b77440284c0f7224
toan.luongcong <60191207+toan.luongcong@users.noreply.github.com>
2020-07-13 14:35:38 +0700

    +ad

--- st/main.3815e.js
+++ st/main.3815e.js
@@ -180,7 +180,7 @@ console.log('=============lcton======');
 if (window.jsb) {
     var isRuntime = (typeof loadRuntime === 'function');
     if (isRuntime) {
-        require('src/settings.2c9c0.js');
+        require('src/settings.2c355.js');
         require('src/cocos2d-runtime.js');
         if (CC_PHYSICS_BUILTIN || CC_PHYSICS_CANNON) {
             require('src/physics.js');
@@ -188,7 +188,7 @@ if (window.jsb) {
         require('jsb-adapter/engine/index.js');
     }
     else {
-        require('src/settings.2c9c0.js');
+        require('src/settings.2c355.js');
         require('src/cocos2d-jsb.js');
         if (CC_PHYSICS_BUILTIN || CC_PHYSICS_CANNON) {
             require('src/physics.js');
